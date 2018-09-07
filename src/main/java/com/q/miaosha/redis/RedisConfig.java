09a626a22f01ddb4ff0250af8f74b385ef5c16ed
qiu <369971654@qq.com>
2018-09-07 13:27:39 +0800

    集成redis和druid

---
+++ src/main/java/com/q/miaosha/redis/RedisConfig.java
@@ -0,0 +1,50 @@
+package com.q.miaosha.redis;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2018/9/7 13:23
+ * @Description:
+ */
+@Component
+@ConfigurationProperties(prefix="redis")
+public class RedisConfig {
+    private String host;
+    private int port;
+    private int timeout;//秒
+    private String password;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/q/miaosha/redis/RedisService.java
@@ -0,0 +1,9 @@
+package com.q.miaosha.redis;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2018/9/7 13:25
+ * @Description:
+ */
+public class RedisService {
+}
